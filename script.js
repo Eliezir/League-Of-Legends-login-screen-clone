2af8beafba9ff1234a5113632dfc7b7c59339501
Eliezir Moreira <eliezir2004@gmail.com>
2022-06-27 21:54:09 -0300

    live wallpaper - show password

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.login__button');
+const password_icon = document.querySelector(".password__visible");
+const visibility_icon = document.querySelector(".visibility");
+const password_label = document.querySelector("#password");
+
+var checkBoxEffects = document.querySelector(".effects--input--checkbox");
+
+
+checkBoxEffects.addEventListener('change',function() {
+if(checkBoxEffects.checked){
+  document.querySelector("#wallpaper-live").classList.add("wallpaper-static")
+}
+else{
+  document.querySelector("#wallpaper-live").classList.remove("wallpaper-static")
+}})
 
 const handleFocus = ({target}) => {
-  const span = target.previousElementSibling;
+  if(target.type==="button"){
+    target = inputs[1]}
+  const span = target.parentElement.firstElementChild;
   span.classList.add('span-active');  
+  if(target.name == "password"){
+    password_icon.classList.add('password__visible-active')
+  }
 }
+
 const handleFocusOut = ({target}) => {
-  if(target.value ===''){
-  const span = target.previousElementSibling;
-  span.classList.remove('span-active');}
+  if(target.type==="button"){
+    target = inputs[1]}
+  else if(target.value ===''){
+    const span = target.parentElement.firstElementChild;
+    span.classList.remove('span-active');}
+      
 }
+
+document.querySelector("body").addEventListener('click',function(e){
+  var fora = !password_label.contains(e.target);
+    if(fora){
+      password_icon.classList.remove('password__visible-active')
+    }
+})
+
 
 const handleChange = () => {
   const[username , password] = inputs;
@@ -22,6 +53,23 @@ else{
 }
 }
 
+var password = true;
+
+
+password_icon.addEventListener('click', function() {
+  if (password) {
+    inputs[1].setAttribute('type', 'text');
+    visibility_icon.innerHTML = 'visibility';
+  } else {
+    inputs[1].setAttribute('type', 'password');
+    visibility_icon.innerHTML = 'visibility_off';
+  }
+  password = !password;
+  
+});
+
 inputs.forEach((input) => input.addEventListener('focus',handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout',handleFocusOut));
+password_icon.addEventListener('focus',handleFocus)
+password_icon.addEventListener('focusout',handleFocusOut)
 inputs.forEach((input) => input.addEventListener('input',handleChange));
